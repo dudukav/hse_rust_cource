158d8e1979f1cc450759eb2ad27e13db9740b91c
Виктория Кашуркина <kvictorijas@gmail.com>
2024-10-01 11:29:27 +0300

    solution

---
+++ ownerships/where_k_th_ordinal_element_greater/src/lib.rs
@@ -0,0 +1,18 @@
+#![forbid(unsafe_code)]
+
+pub fn where_k_th_ordinal_element_greater<'a>(lhs: &'a Vec<i32>, rhs: &'a Vec<i32>, k: usize) -> &'a Vec<i32> {
+    if k >= lhs.len() || k >= rhs.len() {
+        return  rhs;
+    }
+
+    let mut lhs_copy = lhs.clone();
+    let mut rhs_copy = rhs.clone();
+    lhs_copy.sort();
+    rhs_copy.sort();
+
+    if lhs_copy[k] > rhs_copy[k] {
+        return lhs;
+    } else {
+        return rhs;
+    }
+}
